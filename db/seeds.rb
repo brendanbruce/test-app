f225d7587c217be351d83c78bdebf0db2e3278f0
Brendan Bruce <brendan@skilledup.com>
2015-04-07 21:06:08 -0400

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,48 @@
 require 'faker'
 
-50.times do
-  Post.create!(
-    title: Faker::Lorem.sentence,
-    body: Faker::Lorem.paragraph
-  )
+if Post.count < 50
+  50.times do
+    Post.create!(
+      title: Faker::Lorem.sentence,
+      body: Faker::Lorem.paragraph
+    )
+  end
 end
+
 posts = Post.all
 
-100.times do
-  Comment.create!(
-    post: posts.sample,
-    body: Faker::Lorem.paragraph
+if Comment.count < 100
+  100.times do
+    Comment.create!(
+      post: posts.sample,
+      body: Faker::Lorem.paragraph
+    )
+  end
+end
+
+p = Post.find_by title: "We Guns"
+
+if p
+  puts "We Guns already created"
+else
+  Post.create!(
+    title: "We Guns",
+    body: "Machine gun kelly"
   )
 end
+
+
+c = Comment.find_by body: "You Suck!"
+
+if c
+  puts "You Suck already created!"
+else
+  Comment.create!(
+    post: posts.sample,
+    body: "You Suck!"
+  )
+end
+
 
 puts "Seed finished"
 puts "#{Post.count} posts created"
